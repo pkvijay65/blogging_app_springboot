4b896afeafa0855b70bc5e6bdc448fbb5d3560d6
Prateek Vijay <113239008+pkvijay65@users.noreply.github.com>
2023-03-18 19:44:45 +0530

    initial project setup

---
+++ src/main/java/com/blogging/blogapplication_springboot/BlogApplicationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.blogging.blogapplication_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplicationSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplicationSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blogging/blogapplication_springboot/BlogApplicationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogging.blogapplication_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
